79d1e8740d507ff2f35e9ae9b4ff0c158a7f2696
Ingo Richter <ingo.richter+github@gmail.com>
2024-09-22 22:42:02 -0700

    integration test and health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let listener = std::net::TcpListener::bind("0.0.0.0:8000")?;
+    run(listener)?.await
 }
