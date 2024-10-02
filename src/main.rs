07f0d4f8ee241830ce56548db3d847dfa2aa226e
Ingo Richter <ingo.richter+github@gmail.com>
2024-10-01 22:45:39 -0700

    add logging and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::io::Error;
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration file.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string()).await.expect("Failed to connect to Postgres.");
